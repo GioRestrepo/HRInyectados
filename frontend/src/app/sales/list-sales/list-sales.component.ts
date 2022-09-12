e3d36704ab72b5a2ba6d8443dd855d3d9a43491a
GioRestrepo <gierestrepove@unal.edu.co>
2022-09-11 21:04:44 -0500

    list-sales

--- frontend/src/app/sales/list-sales/list-sales.component.ts
+++ frontend/src/app/sales/list-sales/list-sales.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CustomersService } from 'src/app/services/customers.service';
+import { SalesService } from 'src/app/services/sales.service';
 import { UtilService } from 'src/app/services/util.service';
 
 @Component({
@@ -12,22 +13,47 @@ export class ListSalesComponent implements OnInit {
   showResults: boolean = false;
   selectedId: any;
   salesByUser: any = [];
-  constructor(private _customerService: CustomersService, private _utilsService: UtilService) { }
+ 
+  constructor(
+    private _customerService: CustomersService, 
+    private _utilsService: UtilService,
+    private _saleService: SalesService
+    ) 
+    { }
 
   ngOnInit(): void {
     this._customerService.listCustomers().subscribe(
       (res) => {
         this.customers = res;
+        
       },
       (err) =>{
         this._utilsService.openSnackBarError("Ha ocurrido un error al cargar los clientes")
       }
     )
+
+    
   }
+
+
 
   searchSalesByCustomer(id: any){
     console.log(id);
-    
+    this._saleService.listSale(id).subscribe(
+      (res) => {
+        this.salesByUser = res;
+        if(this.salesByUser.length != 0){
+          this.showResults = true
+        } else {
+          this.showResults = false
+        }
+        console.log(this.salesByUser);
+        
+      },
+      (err) => {
+        this._utilsService.openSnackBarError("Ha ocurrido un error al cargar las ventas")
+      }
+    )
   }
 
 }
--- frontend/src/app/services/sales.service.ts
+++ frontend/src/app/services/sales.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SalesService {
-
-  constructor() { }
+  env: String;
+  constructor(private _httpClient: HttpClient) { 
+    this.env = environment.urlAPI;
+  }
+  createSale(data: any){
+    return this._httpClient.post<any>(`${this.env}ventas`, data);
+  }
+  listSale(id: number = 0){
+    return this._httpClient.get<any>(
+      `${this.env}ventas/${
+        id != 0
+          ? id 
+          : ''
+      }`
+    );
+  }
 }
